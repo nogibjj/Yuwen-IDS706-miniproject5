417135da7e7d9526a50b543b3c08f484d47e4758
Candice1121 <75481901+Candice1121@users.noreply.github.com>
2023-10-01 13:45:11 -0400

    upload

--- main.py
+++ main.py
@@ -1,2 +1,87 @@
-def addfunction(a,b):
-    return a + b
+import sqlite3
+
+# Function to create a database connection
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+
+# Function to create the 'users' table if it doesn't exist
+def create_table(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                username TEXT NOT NULL,
+                email TEXT NOT NULL
+            )
+        ''')
+    except sqlite3.Error as e:
+        print(e)
+
+# Function to insert a new user into the database
+def insert_user(conn, username, email):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (username, email) VALUES (?, ?)", (username, email))
+        conn.commit()
+    except sqlite3.Error as e:
+        print(e)
+
+# Function to retrieve a user by username
+def get_user_by_username(conn, username):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+        return cursor.fetchone()
+    except sqlite3.Error as e:
+        print(e)
+
+# Function to update a user's email by username
+def update_user_email(conn, username, new_email):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("UPDATE users SET email=? WHERE username=?", (new_email, username))
+        conn.commit()
+    except sqlite3.Error as e:
+        print(e)
+
+# Function to delete a user by username
+def delete_user(conn, username):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM users WHERE username=?", (username,))
+        conn.commit()
+    except sqlite3.Error as e:
+        print(e)
+
+if __name__ == "__main__":
+    database_file = "my_database.db"
+    connection = create_connection(database_file)
+    if connection:
+        create_table(connection)
+
+        # Create a new user
+        insert_user(connection, "JohnDoe", "john@example.com")
+
+        # Read a user
+        user = get_user_by_username(connection, "JohnDoe")
+        if user:
+            print("User found:", user)
+
+        # Update user email
+        update_user_email(connection, "JohnDoe", "new_email@example.com")
+
+        # Read the updated user
+        updated_user = get_user_by_username(connection, "JohnDoe")
+        if updated_user:
+            print("Updated user:", updated_user)
+
+        # Delete the user
+        delete_user(connection, "JohnDoe")
+
+        connection.close()
--- test_main.py
+++ test_main.py
@@ -1,11 +1,43 @@
-from main import addfunction
+from main import create_connection, create_table, insert_user, get_user_by_username, update_user_email, delete_user
 
-def test_add():
-    """Function calling addfunction"""
+def test_insert_user():
+    database_file = "test_database.db"
+    connection = create_connection(database_file)
+    create_table(connection)
 
-    a = 0
-    b = 1
-    c = 2
-    assert addfunction(a,b) == 1
-    assert addfunction(a,c) == 2
-    assert addfunction(b,c) == 3
+    insert_user(connection, "AliceSmith", "alice@example.com")
+    user = get_user_by_username(connection, "AliceSmith")
+    connection.close()
+
+    assert user is not None
+    assert user[2] == "alice@example.com"
+
+def test_update_user_email():
+    database_file = "test_database.db"
+    connection = create_connection(database_file)
+    create_table(connection)
+
+    insert_user(connection, "BobJohnson", "bob@example.com")
+    update_user_email(connection, "BobJohnson", "new_email@example.com")
+    user = get_user_by_username(connection, "BobJohnson")
+    connection.close()
+
+    assert user is not None
+    assert user[2] == "new_email@example.com"
+
+def test_delete_user():
+    database_file = "test_database.db"
+    connection = create_connection(database_file)
+    create_table(connection)
+
+    insert_user(connection, "CharlieBrown", "charlie@example.com")
+    delete_user(connection, "CharlieBrown")
+    user = get_user_by_username(connection, "CharlieBrown")
+    connection.close()
+
+    assert user is None
+
+if __name__ == "__main__":
+    test_insert_user()
+    test_update_user_email()
+    test_delete_user()
